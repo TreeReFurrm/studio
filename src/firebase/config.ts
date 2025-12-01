766a0f3b7c93852450b64a6b85bcf898ed88a089
Teresa Furr <harleygirley97@gmail.com>
2025-12-01 07:03:55 +0000

    File changes

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-  "projectId": "studio-9511907405-4ef32",
+  "projectId": "studio-4760471421",
   "appId": "1:681226570708:web:052f4aa90e74ec9d456441",
   "apiKey": "AIzaSyBnle5X2L5Oh6pcov2AehPw264zRxrmtEM",
-  "authDomain": "studio-9511907405-4ef32.firebaseapp.com",
+  "authDomain": "studio-4760471421.firebaseapp.com",
   "measurementId": "",
   "messagingSenderId": "681226570708"
 };
